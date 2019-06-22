03fc86a43219300487e79c4f9501d6deb4d9eea2
Spurs <2906394727@qq.com>
2019-06-22 16:04:37 +0800

    add dtype

--- pandas_1.py
+++ pandas_1.py
@@ -2,8 +2,9 @@ import numpy as np
 
 array = np.array([[1,2,3],
                   [4,5,6],
-                  [[4,7],[7,8],[5,4]]])
+                  [7,8,9]],dtype='int64')
 print(array)
 print(array.ndim) #判断几维数组的方法便是看中括号的位置对数有关
 print(array.shape)
 print(array.size) #元素的个数
+print(array.dtype)#元素的数据类型 整型默认为int32
